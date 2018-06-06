4ff5bdc532812494e319a3c7cf5f7b0203501da0
Marek Zuzi <zuzi.marek@gmail.com>
2018-06-06 14:32:54 +0200

    Adding progress - assets etc

---
+++ Assets/Scripts/Excavation.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Excavation : MonoBehaviour {
+
+    public float excavationSpeed;
+    public float excavationReach;
+
+    public GameObject excavator;
+    public Terrain seafloor;
+
+    private const int EXCAVATE_SIZE = 15;
+    private const int EXCAVATE_SIZE_2 = EXCAVATE_SIZE / 2;
+
+    // Use this for initialization
+    void Start () {}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // EXCAVATION
+        if (Input.GetButton("Fire1") || Input.GetButton("Fire2"))
+        {
+            // decide whether adding or subtracting sand
+            float modifier = 1.0f;
+            if (Input.GetButton("Fire2"))
+            {
+                modifier = -1.0f;
+            }
+
+            // do the ray cast to see where excavating
+            RaycastHit hit;
+            Ray ray = new Ray(excavator.transform.position, excavator.transform.TransformDirection(Vector3.up));
+            if (Physics.Raycast(ray, out hit, excavationReach))
+            {
+                Debug.Log("ray cast hit at" + hit.point + " which is at uv " + hit.textureCoord + " and terrain has size " + seafloor.terrainData.size + " and heightmap width " + seafloor.terrainData.heightmapWidth);
+
+                // calculate the point where the ray hit by using UV coordinates and heightmap resolution
+                Vector3 hitpoint = new Vector3
+                {
+                    x = (hit.textureCoord.x * seafloor.terrainData.heightmapWidth),
+                    y = 1.0f,
+                    z = (hit.textureCoord.y * seafloor.terrainData.heightmapWidth)
+                };
+
+                // get the height samples from the right spot
+                float[,] samples = seafloor.terrainData.GetHeights(
+                    Mathf.CeilToInt(hitpoint.x)-EXCAVATE_SIZE_2,
+                    Mathf.CeilToInt(hitpoint.z)-EXCAVATE_SIZE_2,
+                    EXCAVATE_SIZE,
+                    EXCAVATE_SIZE);
+
+                // compute the change in each height sample
+                for (int i = 0; i < EXCAVATE_SIZE; i++)
+                {
+                    for (int j = 0; j < EXCAVATE_SIZE; j++)
+                    {
+                        // local sample coordinates in the patch, centered in the middle
+                        int sample_x = i - EXCAVATE_SIZE_2;
+                        int sample_y = j - EXCAVATE_SIZE_2;
+
+                        // use quadratic function to get amount of change in this sample
+                        float a = 1.5f;
+                        float b = a;
+                        float c = 1.0f / EXCAVATE_SIZE_2;
+                        float z = ((sample_x * sample_x * c) / (a * a)) + ((sample_y * sample_y * c) / (b * b));
+
+                        float sample_amount = Mathf.Clamp(1.0f - z, 0.0f, 1.0f);
+
+                        // update samples by combining sample change amount, speed, time, and switch for add/subtract
+                        samples[i, j] = samples[i, j] + (sample_amount * modifier * excavationSpeed * Time.deltaTime);
+                    }
+                }
+
+                // update the height samples
+                seafloor.terrainData.SetHeights(
+                    Mathf.CeilToInt(hitpoint.x)-EXCAVATE_SIZE_2,
+                    Mathf.CeilToInt(hitpoint.z)-EXCAVATE_SIZE_2,
+                    samples);
+            }
+        }
+    }
+}
--- Assets/Scripts/FreeCamera.cs
+++ Assets/Scripts/FreeCamera.cs
@@ -4,75 +4,56 @@ using UnityEngine;
 
 public class FreeCamera : MonoBehaviour {
 
-    public float speed;
+    public float lookSpeed;
+    public float moveSpeed;
+    public float excavationSpeed;
     public Terrain Ter;
 
     private float horizontalAngle;
     private float verticalAngle;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private const int EXCAVATE_SIZE = 15;
+    private const int EXCAVATE_SIZE_2 = EXCAVATE_SIZE / 2;
+    private const float LOOK_LIMIT = 70;
+
+    // Use this for initialization
+    void Start () {}
 	
 	// Update is called once per frame
 	void Update () {
-        horizontalAngle += Input.GetAxis("Mouse X") * speed;
-        verticalAngle += Input.GetAxis("Mouse Y") * speed;
+        // FREE CAMERA LOOK
+        horizontalAngle += Input.GetAxis("Mouse X") * lookSpeed;
+        verticalAngle += Input.GetAxis("Mouse Y") * lookSpeed;
 
         constrainCamera();
         Quaternion q = Quaternion.AngleAxis(horizontalAngle, Vector3.up) * Quaternion.AngleAxis(verticalAngle, -Vector3.right);
-
         transform.rotation = q;
 
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
-        transform.Translate(new Vector3(h, 0.0f, v));
-
-        // EXCAVATION
-        if (Input.GetButtonDown("Fire1"))
+        // MOVEMENT
+        Vector3 translation = new Vector3
         {
-            Debug.Log("fire pressed");
-            RaycastHit hit;
+            x = Input.GetAxis("Horizontal"),
+            y = Input.GetAxis("Jump"),
+            z = Input.GetAxis("Vertical")
+        };
+        transform.Translate(translation * moveSpeed);
 
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
-            {
-                Debug.Log("ray cast hit at" + hit.point + " which is at uv " + hit.textureCoord + " and texture has this " + Ter.terrainData.detailWidth);
-                int SIZ = 15;
-                int SIZ2 = 7;
-                float maxRad = Mathf.Sqrt(2 * (SIZ2 * SIZ2));
-
-                Vector3 hitpoint = hit.point - Ter.transform.position;
-
-                float[,] samples = Ter.terrainData.GetHeights(Mathf.CeilToInt(hitpoint.x), Mathf.CeilToInt(hitpoint.z), SIZ, SIZ);
-                for (int i = 0; i < SIZ; i++)
-                {
-                    for (int j = 0; j < SIZ; j++)
-                    {
-                        float radius = Mathf.Sqrt(Mathf.Pow(Mathf.Abs(i - (SIZ / 2)), 2) + Mathf.Pow(Mathf.Abs(j - (SIZ / 2)), 2));
-                        radius = Mathf.Abs(radius - maxRad) / maxRad;
-                        samples[i, j] = samples[i, j] + 0.01f * radius;
-
-                        //samples[i, j] = samples[i, j] + 0.1f * Time.deltaTime;
-                    }
-                }
-
-                Ter.terrainData.SetHeights(Mathf.CeilToInt(hitpoint.x), Mathf.CeilToInt(hitpoint.z), samples);
-            }
-        }
+        // FOG based on depth
+        //RenderSettings.fogDensity = 0.045f + Mathf.Max(10.0f - transform.position.y, 0.0f)*0.05f;
     }
 
     void constrainCamera()
     {
-        if(horizontalAngle > 360.0f)
+        // make horizontal angle be in range of 360 degrees
+        if (horizontalAngle > 360.0f)
         {
             horizontalAngle -= 360.0f;
         }
-        if(horizontalAngle < -360.0f)
+        if (horizontalAngle < -360.0f)
         {
             horizontalAngle += 360.0f;
         }
-        Mathf.Clamp(verticalAngle, -45.0f, 45.0f);
+        // make vertical angle be between -LOOK_LIMIT and LOOK_LIMIT
+        verticalAngle = Mathf.Clamp(verticalAngle, -LOOK_LIMIT, LOOK_LIMIT);
     }
 }
